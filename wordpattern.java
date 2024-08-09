476129027751da5cab4c6928c7b1e67874ed53f4
Shrey <guptashrey163@gmail.com>
2024-08-09 22:01:31 +0530

    daily dsa

---
+++ wordpattern.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        Map<Character, String> pToStr = new HashMap<>();
+        Map<String, Character> strToP = new HashMap<>();
+        String[] words = s.split(" ");
+        if (words.length != pattern.length()) {
+            return false;
+        }
+        for (int i = 0; i < pattern.length(); i++) {
+            char pChar = pattern.charAt(i);
+            String word = words[i];
+            // Check if there's a conflict in the mapping from pattern to word
+            if (pToStr.containsKey(pChar) && !pToStr.get(pChar).equals(word)) {
+                return false;
+            }
+            if (strToP.containsKey(word) && strToP.get(word) != pChar) {
+                return false;
+            }
+            pToStr.put(pChar, word);
+            strToP.put(word, pChar);
+        }
+        return true;
+    }
+}
