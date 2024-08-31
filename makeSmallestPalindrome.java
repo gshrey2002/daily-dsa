b35c81de3fdb3cd09288039ab1be4fb17dca28e7
Shrey <guptashrey163@gmail.com>
2024-08-31 13:37:02 +0530

    daily dsa

---
+++ makeSmallestPalindrome.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String makeSmallestPalindrome(String s) {
+        int i=0;
+        int j=s.length()-1;
+        char[] arr = s.toCharArray();
+        while(i<=j){
+            if(arr[i]!=arr[j]){
+                arr[i] = arr[j] = (char) Math.min(arr[i],arr[j]);
+            }
+            i++;
+            j--;
+        }
+        return new String(arr);
+    }
+}
