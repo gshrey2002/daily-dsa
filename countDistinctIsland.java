b73c09f9583a60218fa9da63000f107e1ea47e88
Shrey <guptashrey163@gmail.com>
2024-09-23 15:39:42 +0530

    count distinct island

---
+++ countDistinctIsland.java
@@ -0,0 +1,108 @@
+class Pair{
+    int first;
+    int second;
+    public Pair(int first,int second){
+        this.first=first;
+        this.second=second;
+    }
+}
+class Solution {
+    
+    private void dfs(int row,int col,int vis[][],int grid[][],ArrayList<String> vec,int row0,int col0){
+        vis[row][col]=1;
+        vec.add(toString(row-row0,col-col0));
+        int n=grid.length;
+        int m=grid[0].length;
+        
+        int delRow[]={-1,0,+1,0};
+        int delCol[]={0,-1,0,+1};
+        
+        for(int i=0;i<4;i++){
+            int nrow=row+delRow[i];
+            int ncol=col+delCol[i];
+            
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
+                dfs(nrow,ncol,vis,grid,vec,row0,col0);
+            }
+        }
+        
+    }
+    
+    private String toString(int r,int c){
+        return Integer.toString(r)+" "+Integer.toString(c);
+    }
+
+    int countDistinctIslands(int[][] grid) {
+        
+        HashSet<ArrayList<String>> st=new HashSet<>();
+        int n=grid.length;
+        int m=grid[0].length;
+        int vis[][]=new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(vis[i][j]==0 && grid[i][j]==1){
+                    ArrayList<String> vec=new ArrayList<>();
+                    dfs(i,j,vis,grid,vec,i,j);
+                    st.add(vec);
+                    
+                }
+            }
+        }
+        // Your Code here
+        return st.size();
+    }
+}
+class Pair{
+    int first;
+    int second;
+    public Pair(int first,int second){
+        this.first=first;
+        this.second=second;
+    }
+}
+class Solution {
+    
+    private void dfs(int row,int col,int vis[][],int grid[][],ArrayList<String> vec,int row0,int col0){
+        vis[row][col]=1;
+        vec.add(toString(row-row0,col-col0));
+        int n=grid.length;
+        int m=grid[0].length;
+        
+        int delRow[]={-1,0,+1,0};
+        int delCol[]={0,-1,0,+1};
+        
+        for(int i=0;i<4;i++){
+            int nrow=row+delRow[i];
+            int ncol=col+delCol[i];
+            
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
+                dfs(nrow,ncol,vis,grid,vec,row0,col0);
+            }
+        }
+        
+    }
+    
+    private String toString(int r,int c){
+        return Integer.toString(r)+" "+Integer.toString(c);
+    }
+
+    int countDistinctIslands(int[][] grid) {
+        
+        HashSet<ArrayList<String>> st=new HashSet<>();
+        int n=grid.length;
+        int m=grid[0].length;
+        int vis[][]=new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(vis[i][j]==0 && grid[i][j]==1){
+                    ArrayList<String> vec=new ArrayList<>();
+                    dfs(i,j,vis,grid,vec,i,j);
+                    st.add(vec);
+                    
+                }
+            }
+        }
+        // Your Code here
+        return st.size();
+    }
+}
